be96d0c35afd47bbb871978bc8071f5836eae201
Master-Hash <A137294381b@163.com>
2022-11-10 21:23:54 +0800

    perf: remove useless line `LEN`

--- 6-1.py
+++ 6-1.py
@@ -6,7 +6,6 @@ with open("外文歌词.txt", encoding="utf-8") as f:
     # 读取外文歌词到列表变量lyric_foreign中
     LYRIC_FOREIGN = map(lambda i: i.strip(), f.readlines())
 
-LEN = 15
 for zh, foreign in zip(LYRIC_ZH, LYRIC_FOREIGN):
     # 逐行打印中文歌词和外文歌词
     print(zh)
